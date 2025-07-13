98782b0cc065787135019e649226ed00f0417b71
KayyLiaa <haekalhawaari35@gmail.com>
2025-07-13 21:11:27 +0700

    Tabel Penghuni
    membuat tabel penghuni ke dalam database

---
+++ database_kost.sql
@@ -0,0 +1,10 @@
+CREATE TABLE tb_penghuni (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nama VARCHAR(100),
+    no_ktp VARCHAR(50),
+    no_hp VARCHAR(20),
+    tgl_masuk DATE,
+    tgl_keluar DATE
+);
+
+
